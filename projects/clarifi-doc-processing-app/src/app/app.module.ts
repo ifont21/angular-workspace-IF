b781739df349bdaf5ec02238a97eff63660225ee
Daniel Stiven Restrepo Arcila <daniel@MacBook-Pro-de-Daniel-2.local>
2019-07-10 10:23:08 -0500

    Moving most of dependencies to clirifi-core-ui

--- projects/clarifi-doc-processing-app/src/app/app.module.ts
+++ projects/clarifi-doc-processing-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { FakeComponent } from './fake/fake.component';
 import { ClarifiCoreUiModule } from 'clarifi-core-ui';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@core-dependencies/@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
